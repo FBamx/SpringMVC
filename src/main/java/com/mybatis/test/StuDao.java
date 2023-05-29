0e181246ddb4065462e7144771aae5c4f4d6b214
Elisii <495745954@qq.com>
2023-05-29 15:18:23 +0800

    combine spring and mybatis

--- src/main/java/com/mybatis/test/StuDao.java
+++
@@ -1,7 +0,0 @@
-package com.mybatis.test;
-
-import java.util.List;
-
-public interface StuDao {
-    List<Student> getStudentList();
-}
---
+++ src/test/java/com/demo/test/Test_SpringMybatis.java
@@ -0,0 +1,26 @@
+package com.demo.test;
+
+import com.demo.mapper.UserMapper;
+import com.demo.pojo.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring/SpringMVC.xml")
+public class Test_SpringMybatis {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testFindUserList() {
+        List<User> userList = userMapper.findUserList();
+        System.out.println(userList);
+    }
+
+}
